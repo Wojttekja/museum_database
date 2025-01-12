e4ba482c97a5cfc3211ad83e721ddb746e7e6140
Wojttekja <mierzejekwojciech@gmail.com>
2025-01-12 20:49:27 +0100

    death date can be null

--- exhibits/models.py
+++ exhibits/models.py
@@ -6,15 +6,21 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     birth_year = models.IntegerField()
-    death_year = models.IntegerField(blank=True)
+    death_year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 
 class Artwork(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True, null=True)
     type = models.CharField(max_length=100)
     height = models.IntegerField()
     width = models.IntegerField()
     weight = models.IntegerField()
     valuable = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.title} by {self.artist}"
