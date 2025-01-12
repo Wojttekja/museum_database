7093798a694cf4847a6a59b2c63f764490bd165a
Wojttekja <mierzejekwojciech@gmail.com>
2025-01-12 17:07:56 +0100

    now revert

--- museum_database/wsgi.py
+++ museum_database/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'museum_database.settings')
 
 application = get_wsgi_application()
-
-from helloworld.wsgi import HelloWorldApplication
-
-application = HelloWorldApplication(application)
