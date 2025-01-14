df517279610f6b05b294a664acc33f1b638c4862
Wojttekja <mierzejekwojciech@gmail.com>
2025-01-14 20:25:55 +0100

    update database

--- exhibits/models.py
+++ exhibits/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Artist(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -24,3 +24,32 @@ class Artwork(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
+
+
+class Places(models.Model):
+    PLACE_TYPES = {
+        "W muzeum": "internal",
+        "Wypożyczone": "external",
+    }
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=100, choices=PLACE_TYPES)
+
+
+class OutsidePlaces(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    id_place = models.ForeignKey(Places, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} in {self.city}"
+
+
+class InsidePlaces(models.Model):
+    id = models.AutoField(primary_key=True)
+    room = models.CharField(max_length=100)
+    localization_in = models.ForeignKey("self", blank=True, null=True)
+    id_place = models.ForeignKey(Places, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.room}"
