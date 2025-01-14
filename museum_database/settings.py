0b6d2662d2b5a73af254515204392c2688c02ef7
Wojttekja <mierzejekwojciech@gmail.com>
2025-01-14 19:16:01 +0100

    shudder before the beautiful

--- museum_database/settings.py
+++ museum_database/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6zm1=c9xyw^0@afd%ivow4$c!o%@r%=np$yry3tpij*m7c+8%p'
+with open ('run/secrets/secret_key', 'r') as file:
+    SECRET_KEY = file.read().replace('\n', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
