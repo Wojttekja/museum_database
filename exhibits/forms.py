a1af2dfe658d6e3e6ce3ccaa545424646cc20064
Wojttekja <mierzejekwojciech@gmail.com>
2025-01-25 19:55:24 +0100

    endless forms most beautiful

--- exhibits/forms.py
+++ exhibits/forms.py
@@ -3,13 +3,13 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
+    username = forms.CharField(label="Nazwa Użytkownika", widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'Username'
     }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Password'
+        'placeholder': 'Hasło',
     }))
 
 
@@ -22,8 +22,8 @@ class ArtistForm(forms.ModelForm):
 
 from .models import Artwork
 class ArtworkstFilterForm(forms.Form):
-    title = forms.CharField(max_length=100, required=False)
-    artist = forms.ModelChoiceField(queryset=Artist.objects.all(), required=False)
+    title = forms.CharField(max_length=100, required=False, label="Tytuł")
+    artist = forms.ModelChoiceField(queryset=Artist.objects.all(), required=False, label="Autor")
 
 
 from .models import OutsidePlaces
